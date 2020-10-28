f37b6fd74a7a1e0130aef1469d70ed5436084e9f
Act-KNoT <73528550+anan-knot@users.noreply.github.com>
2020-10-29 00:33:22 +0900

    Update index.js

--- examples/gallery/index.js
+++ examples/gallery/index.js
@@ -114,6 +114,87 @@ var scenes = {
   }
 };
 
+
+
+
+function onLoad() {
+  vrView = new VRView.Player('#vrview', {
+    image: 'blank.png',
+    preview: 'blank.png',
+    is_stereo: false,
+    is_autopan_off: true
+  });
+
+  vrView.on('ready', onVRViewReady);
+  vrView.on('modechange', onModeChange);
+  vrView.on('click', onHotspotClick);
+  vrView.on('error', onVRViewError);
+  vrView.on('getposition', onGetPosition);
+}
+
+function onVRViewReady(e) {
+  console.log('onVRViewReady');
+  loadScene('classroom');
+}
+
+function onModeChange(e) {
+  console.log('onModeChange', e.mode);
+}
+
+function onGetPosition(e) {
+  console.log(e);
+
+}
+
+function onHotspotClick(e) {
+  vrView.getPosition()
+  console.log('onHotspotClick', e.id);
+  if (e.id) {
+    loadScene(e.id);
+  }
+}
+
+function loadScene(id) {
+  console.log('loadScene', id);
+
+  // Set the image
+  vrView.setContent({
+    image: scenes[id].image,
+    preview: scenes[id].preview,
+    is_stereo: false,
+    is_autopan_off: true
+  });
+
+  // Add all the hotspots for the scene
+  var newScene = scenes[id];
+  var sceneHotspots = Object.keys(newScene.hotspots);
+  for (var i = 0; i < sceneHotspots.length; i++) {
+    var hotspotKey = sceneHotspots[i];
+    var hotspot = newScene.hotspots[hotspotKey];
+
+    vrView.addHotspot(hotspotKey, {
+      pitch: hotspot.pitch,
+      yaw: hotspot.yaw,
+      radius: hotspot.radius,
+      distance: hotspot.distance
+    });
+  }
+}
+
+function onVRViewError(e) {
+  console.log('Error! %s', e.message);
+}
+
+window.addEventListener('load', onLoad);
+
+
+
+
+
+
+
+
+
 function onLoad() {
   vrView = new VRView.Player('#vrview', {
     width: '100%',
@@ -123,20 +204,31 @@ function onLoad() {
     is_autopan_off: true
   });
 
-  function onLoad() {
-  vrView = new VRView.Player('#vrview', {
-    image: 'blank.png',
-    preview: 'blank.png',
-    is_stereo: false,
-    is_autopan_off: true
-  });
-  
   vrView.on('ready', onVRViewReady);
   vrView.on('modechange', onModeChange);
-  vrView.on('click', onHotspotClick);
-  vrView.on('error', onVRViewError);
   vrView.on('getposition', onGetPosition);
-  
+  vrView.on('error', onVRViewError);
+}
+
+function loadScene(id) {
+  console.log('loadScene', id);
+
+  // Set the image
+  vrView.setContent({
+    image: scenes[id].image,
+    preview: scenes[id].preview,
+    is_autopan_off: true
+  });
+
+  // Unhighlight carousel items
+  var carouselLinks = document.querySelectorAll('ul.carousel li a');
+  for (var i = 0; i < carouselLinks.length; i++) {
+    carouselLinks[i].classList.remove('current');
+  }
+    vrView.getPosition();
+  // Highlight current carousel item
+  document.querySelector('ul.carousel li a[href="#' + id + '"]')
+      .classList.add('current');
 }
 
 function onVRViewReady(e) {
@@ -161,59 +253,12 @@ function onModeChange(e) {
   console.log('onModeChange', e.mode);
 }
 
-function onGetPosition(e) {
-    console.log(e)
-}
-
-function onHotspotClick(e) {
-  vrView.getPosition()
-  console.log('onHotspotClick', e.id);
-  if (e.id) {
-    loadScene(e.id);
-  }
-}
-
-
-function loadScene(id) {
-  console.log('loadScene', id);
-
-  // Add all the hotspots for the scene
-  var newScene = scenes[id];
-  var sceneHotspots = Object.keys(newScene.hotspots);
-  for (var i = 0; i < sceneHotspots.length; i++) {
-    var hotspotKey = sceneHotspots[i];
-    var hotspot = newScene.hotspots[hotspotKey];
-
-    vrView.addHotspot(hotspotKey, {
-      pitch: hotspot.pitch,
-      yaw: hotspot.yaw,
-      radius: hotspot.radius,
-      distance: hotspot.distance
-    });
-  }
-
-  
-  // Set the image
-  vrView.setContent({
-    image: scenes[id].image,
-    preview: scenes[id].preview,
-    is_autopan_off: true
-  });
-  // Unhighlight carousel items
-  var carouselLinks = document.querySelectorAll('ul.carousel li a');
-  for (var i = 0; i < carouselLinks.length; i++) {
-    carouselLinks[i].classList.remove('current');
-  }
-    vrView.getPosition();
-  // Highlight current carousel item
-  document.querySelector('ul.carousel li a[href="#' + id + '"]')
-      .classList.add('current');
-}
-
 function onVRViewError(e) {
   console.log('Error! %s', e.message);
 }
 
-
+function onGetPosition(e) {
+    console.log(e)
+}
 
 window.addEventListener('load', onLoad);
